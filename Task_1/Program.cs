83004716df057652d20b143af875c50199cc38b2
Vyacheslav <1gvp@gmail.com>
2022-09-05 22:40:03 +0500

    code for task 2

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter the first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a > b)
+{
+    Console.Write("max=");
+    Console.WriteLine(a);
+    Console.Write("min=");
+    Console.WriteLine(b);
+}
+if(b > a)
+{
+    Console.Write("max=");
+    Console.WriteLine(b);
+    Console.Write("min=");
+    Console.WriteLine(a);
+}
+if(a == b)
+Console.Write("you entered the same numbers, try again!");
+
